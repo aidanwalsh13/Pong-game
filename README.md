c2cc9996fb17e292b839188aae4f1d1aa4bc7517
aidanwalsh13 <aidanwalsh_13@hotmail.com>
2025-12-22 14:31:16 +0000

    Added colors*3 to players and ball. Mapped key to player color change. Collision detection = ball color change. Added physic that player and ball color must match for collision detection

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pong Game 🏓
+
+Install Python from the official Python website.
+Clone this repo. git clone ---?
+
+### Running the game
+
+#### Option 1: Using uv (recommended)
+
+uv run main.py
+
+#### Option 2: Using python
+
+python -m venv .venv
+source .venv/bin/activate   # Windows: .venv\Scripts\activate
+pip install .
+python main.py
+
+
+## Features
+- Classic Pong mechanics
+- Keyboard controls
+- Lightweight and fast
+- 
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
+import os
 import random
-from ball_physics import *
 
 pygame.init()
 
@@ -10,21 +10,80 @@ screen = pygame.display.set_mode((width, height)) # (()) = a single tuple for th
 
 clock = pygame.time.Clock() # pygame has a Clock OBJECT
 
+ball1 = pygame.Rect(925, 500, 10, 10)
+    # the Rect class is still usable for circle. we initialise it later as a circle shape in the update
+    # define objects OUTSIDE of the game loop, else the update will send them back to start position
+
+speed = 500
+
 vx = speed if random.choice([True, False]) else -speed # left or right
 vy = random.uniform(-speed/2, speed/2) # random, up/down, but not as fast as x, so it stays on mostly x trajectory
 
 player1 = pygame.Rect(100, 250, 5, 100) # x, y, w, h
 player2 = pygame.Rect(1800, 150, 5, 100)
     # "R"ect for pygame class
-ball1 = pygame.Rect(925, 500, 10, 10)
-    # the Rect class is still usable for circle. we initialise it later as a circle shape in the update
-    # define objects OUTSIDE of the game loop, else the update will send them back to start position
+
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+player1_color_index = 0
+player2_color_index = 0 # outside the game loop, else they just reset every frame
+ball1_color_index = 0
+
+def collision(ball1, player1, player2, screen, vx, vy, ball1_color_index, colors, player1_color_index, player2_color_index):
+    p1_hit_pos = (ball1.centery - player1.centery) / (player1.height / 2)
+    p2_hit_pos = (ball1.centery - player2.centery) / (player2.height / 2)
+# ball and player are pygame Rect objects by now. .centery is one of the pygame methods
+# .centery calculates the y position (eg 500) and adds the height of the (object / 2) Eg 500 + (10/2) = 505
+# so the hit_pos calculates the distance between the center of the ball and center of the player, then divides by half player height
+
+    if ball1.top < 0:
+        ball1.top = 0 # keeps from clipping through the wall
+        vy *= -1 # reverse trajectory
+        ball1_color_index = ball1_color_index = (ball1_color_index + random.choice([1, -1])) % len(colors)
+
+    elif ball1.bottom > screen.get_height():
+        ball1.bottom = screen.get_height()
+        vy *= -1
+        ball1_color_index = ball1_color_index = (ball1_color_index + random.choice([1, -1])) % len(colors)
+    
+    if ball1.colliderect(player1) and ball1_color_index == player1_color_index:
+        ball1.left = player1.right
+        vx *= -1 # you can change this number so to make the ball speed up the more it bounces
+        vx = max(-3*speed, min(vx, 3*speed)) # this is useful if we increase the speed after each hit, keeping the speed capped
+        vy += p1_hit_pos * 150
+        ball1_color_index = (ball1_color_index + random.choice([1, -1])) % len(colors)
+
+    elif ball1.colliderect(player2) and ball1_color_index == player2_color_index:
+        ball1.right = player2.left
+        vx *= -1
+        vx = max(-3*speed, min(vx, 3*speed))
+        vy += p2_hit_pos * 150
+        ball1_color_index = (ball1_color_index + 1) % len(colors)
+
+    return vx, vy, ball1_color_index
+
+def reset_ball(ball, width, height, speed, ball1_color_index):
+    ball.center = (width // 2, height // 2)
+
+    vx = speed if random.choice([True, False]) else -speed
+    vy = random.uniform(-speed/2, speed/2)
+
+    if abs(vy) < speed * 0.1: # `while` is better than `if`, but makes the game slower because it has to keep checking
+        vy = random.uniform(-speed/2, speed/2)
+    
+    return vx, vy, ball1_color_index
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                player1_color_index = (player1_color_index + 1) % len(colors)
+
+            if event.key == pygame.K_LEFT:
+                player2_color_index = (player2_color_index + 1) % len(colors)
 
     dt = clock.tick(60) / 1000
         # pygame has a tick FUNCTION
@@ -34,6 +93,7 @@ while running:
         player1.y -= (speed * 1.2) * dt
     if keys[pygame.K_s]:
         player1.y += (speed * 1.2) * dt
+    
     if keys[pygame.K_UP]:
         player2.y -= (speed * 1.2) * dt
     if keys[pygame.K_DOWN]:
@@ -44,22 +104,23 @@ while running:
 
     ball1.x += vx * dt
     ball1.y += vy * dt
-    vx, vy = collision(ball1, player1, player2, screen, vx, vy)
+    vx, vy, ball1_color_index = collision(ball1, player1, player2, screen, vx, vy, ball1_color_index, colors, player1_color_index, player2_color_index)
 
     #if collision(ball1, player1, player2, screen, vx, vy) * 5:
     #    ball1.split()
 
         # Check if ball went off left/right
     if ball1.left <= 0:
-        vx, vy = reset_ball(ball1, width, height, speed)
+        vx, vy, ball1_color_index = reset_ball(ball1, width, height, speed, ball1_color_index)
 
     elif ball1.right >= width:
-        vx, vy = reset_ball(ball1, width, height, speed)
+        vx, vy, ball1_color_index = reset_ball(ball1, width, height, speed, ball1_color_index)
 
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 0, 255), player1)
-    pygame.draw.rect(screen, (0, 255, 0), player2)
-    pygame.draw.circle(screen, (255, 0, 0), ball1.center, 10)
+
+    pygame.draw.rect(screen, (colors)[player1_color_index], player1)
+    pygame.draw.rect(screen, (colors)[player2_color_index], player2)
+    pygame.draw.circle(screen, (colors)[ball1_color_index], ball1.center, 10)
         # "r"ect for pygame function
 
     pygame.display.flip()
